b9b59ede96239b68364ba375d0c7a3a65883cef0
KAMILIA AHLAM TAQI BINTI TAQI MOHAMED <24001462@siswa.um.edu.my>
2026-05-04 09:18:12 +0800

    Added runMenu(), printMenu() and handleChoice() methods

--- SmartLibrary.java
+++ SmartLibrary.java
@@ -1,6 +1,67 @@
+import java.util.Scanner;
+
 public class SmartLibrary implements LibraryADT {
     private BookBST catalogue = new BookBST ();
     private BorrowStack borrowStack = new BorrowStack  ();
+
+    public void runMenu (){
+        Scanner sc = new Scanner (System.in);
+        int choice = 0;
+
+        while (choice != 6){
+            printMenu();
+            System.out.println ("Choice : ");
+            if (sc.hasNextInt()){
+                choice = sc.nextInt();
+                handleChoice(choice);
+
+            } else {
+                System.out.println ("Invalid input. Please try again");
+                sc.nextLine();
+            }
+        }
+        sc.close();
+    }
+
+    public void printMenu(){
+        System.out.println ("==== SMART LIBRARY ====");
+        System.out.println ("1. Borrow Book");
+        System.out.println ("2. Return Book");
+        System.out.println ("3. Show Available Books");
+        System.out.println ("4. Show Borrowed Books");
+        System.out.println ("5. Search Book");
+        System.out.println ("6. Exit");
+    }
+
+    public void handleChoice (int choice){
+        Scanner sc = new Scanner (System.in);
+        switch (choice) {
+            case 1 :
+                System.out.println ("Enter ISBN : ");
+                int borrowIsbn = sc.nextInt();
+                sc.nextLine();
+                borrowBook (borrowIsbn);
+
+            case 2 :
+                System.out.println ("Enter ISBN : ");
+                int returnIsbn = sc.nextInt();
+                System.out.println ("Enter title : ");
+                String returnTitle = sc.nextLine();
+                System.out.println ("Enter author : ");
+                String returnAuthor = sc.nextLine();
+                returnBook (returnIsbn, returnTitle, returnAuthor);
+
+            case 3 :
+
+            case 4 :
+
+            case 5 :
+
+            case 6 :
+                System.out.println ("Thank you for using SmartLibrary :D");
+        }
+        sc.close();
+    }
 
     // Borrowing book method
     public void borrowBook(int isbn){
