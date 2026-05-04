398b3b1ff5bafba7fb352984c0fe9dd76bd6399f
KAMILIA AHLAM TAQI BINTI TAQI MOHAMED <24001462@siswa.um.edu.my>
2026-05-04 08:49:38 +0800

    borrowBook method done

--- SmartLibrary.java
+++ SmartLibrary.java
@@ -1,3 +1,18 @@
 public class SmartLibrary implements LibraryADT {
-    
+    private BookBST catalogue = new BookBST ();
+    private BorrowStack borrowStack = new BorrowStack  ();
+
+    // Borrowing book method
+    public void borrowBook(int isbn){
+        Book b = catalogue.search (isbn); // search in catalogue for the book with specific isbn
+
+        if (b==null){   // if book is not available in catalogue
+            System.out.println ("Book not found");
+            return;
+        } else {
+            borrowStack.push (b);   // add to borrowing stack
+            catalogue.remove (b);   // remove from the catalogue
+            System.out.println ("You have successfully borrowed book : " + isbn + ", " + b.title);
+        }
+    }
 }
