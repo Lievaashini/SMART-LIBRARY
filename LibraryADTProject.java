46b07fcf114d49a148d0bdad675e4835c5cf3d21
Lievaashini <lievaashini@gmail.com>
2026-05-04 15:53:17 +0800

    Add method signatures to LibraryADTProject interface

--- LibraryADTProject.java
+++ LibraryADTProject.java
@@ -1,3 +1,13 @@
+package com.mycompany.libraryadtproject;
+
+/**
+ *
+ * @author LENOVO
+ */
 public interface LibraryADTProject {
-    
+    void addBook (int isbn, String title, String author);
+    void searchBook (int isbn);
+    void borrowBook (int isbn);
+    void viewLatestHistory(); 
+} 
 }
