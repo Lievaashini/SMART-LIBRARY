f080816e75c4605d717f2117ff09b2c4d6f581ae
SHAHITYA SHRI A/P SRI KRISHNAH <25006586@siswa.um.edu.my>
2026-05-04 16:13:05 +0800

    Book changes - tigi

--- Book.java
+++ Book.java
@@ -1,3 +1,41 @@
-public class book{
-    // must have isbn, author, title
+public class Book {
+    private int isbn;
+    private String title;
+    private String author;
+    private Book left;
+    private Book right;
+    
+    public Book(int isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+    
+    public int getIsbn() {
+        return isbn;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public Book getLeft() {
+        return left;
+    }
+    
+    public Book getRight() {
+        return right;
+    }
+    
+    public void setLeft(Book left) {
+        this.left = left;
+    }
+    
+    public void setRight (Book right) {
+        this.right = right;
+    }
 }
