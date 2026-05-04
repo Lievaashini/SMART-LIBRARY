55895690d74332b4e9d6d36ee427890e90865d35
Lievaashini <lievaashini@gmail.com>
2026-05-04 15:47:39 +0800

    Add interface LibraryADTProject for library management

--- LibraryADTProject.java
+++ LibraryADTProject.java
@@ -1,3 +1,17 @@
+/*
+    * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.libraryadtproject;
+
+/**
+ *
+ * @author LENOVO
+ */
 public interface LibraryADTProject {
-    
+    void addBook (int isbn, String title, String author);
+    void searchBook (int isbn);
+    void borrowBook (int isbn);
+    void viewLatestHistory(); 
+}
 }
