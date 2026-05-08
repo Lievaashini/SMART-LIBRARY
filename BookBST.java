fd085cedd09e08f77d10cd171b8bf2e29b345ec9
SHAHITYA SHRI A/P SRI KRISHNAH <25006586@siswa.um.edu.my>
2026-05-08 11:20:03 +0800

    insert in BookBst

--- BookBST.java
+++ BookBST.java
@@ -1,3 +1,22 @@
 public class BookBST {
     // methods must have : insert(), search(), delete()
+    private Book root;
+
+    public void insert(int isbn, String title, String author) {
+        root = insertRec(root, new Book(isbn, title, author));
+    }
+
+    private Book insertRec(Book root, Book newBook) {
+        if (root == null) {
+            return newBook;
+        }
+
+        if (newBook.getIsbn() < root.getIsbn()) {
+            root.setLeft(insertRec(root.getLeft(), newBook));
+        } else if (newBook.getIsbn() > root.getIsbn()) {
+            root.setRight(insertRec(root.getRight(), newBook));
+        }
+
+        return root;
+    }
 }
