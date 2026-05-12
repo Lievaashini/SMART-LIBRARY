c2aeb5fa6d77e281a7e92b0f68c528defaa1fecf
WONG XIANG HONG <25060693@siswa.um.edu.my>
2026-05-13 02:19:50 +0800

    Add push and show methods to BorrowStack class
    Implemented push and show methods for managing a stack of books.

--- BorrowStack.java
+++ BorrowStack.java
@@ -1,3 +1,19 @@
+import java.util.Stack;
+
 public class BorrowStack {
-    // methods must have : push(), pop(), show(), peek()
+    private Stack<Book> stack = new Stack<>();
+
+    public void push(Book b) { stack.push(b); }
+
+    public void show() {
+        if (stack.isEmpty()) {
+            System.out.println("History is empty.");
+        } else {
+            // Display LIFO: Most recent first
+            for (int i = stack.size() - 1; i >= 0; i--) {
+                Book b = stack.get(i);
+                System.out.println("[ISBN: " + b.getIsbn() + "] " + b.getTitle());
+            }
+        }
+    }
 }
