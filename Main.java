6848f63bd66b076d019c0ff1b05c700a3157aa85
WONG XIANG HONG <25060693@siswa.um.edu.my>
2026-05-13 09:30:48 +0800

    Improve main method formatting and add comment
    Refactor main method formatting and add comment.

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+package com.mycompany.smartlibraryproject; 
+
 public class Main {
-    public static void main (String [] args){
-        
+    public static void main(String[] args) {
+        // Starts the functional console interface 
+        new SmartLibrary().runMenu();
     }
 }
