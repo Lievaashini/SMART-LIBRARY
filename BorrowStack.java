c1b7f2b2daeaa9122d01ccd94fad7de800dd4e44
WONG XIANG HONG <25060693@siswa.um.edu.my>
2026-05-13 19:36:24 +0800

    Return to original version for you

--- BorrowStack.java
+++ BorrowStack.java
@@ -1,31 +1,3 @@
-import java.util.Stack;
-
 public class BorrowStack {
-    private Stack<Book> stack = new Stack<>();
-
-    public void push(Book b) {
-        if (b != null) stack.push(b);
-    }
-
-    // NEW: Find a book by ISBN to facilitate returning it
-    public Book findAndRemove(int isbn) {
-        for (int i = 0; i < stack.size(); i++) {
-            if (stack.get(i).getIsbn() == isbn) {
-                return stack.remove(i); // Removes from history and returns the book object
-            }
-        }
-        return null;
-    }
-
-    public void show() {
-        if (stack.isEmpty()) {
-            System.out.println("History is empty.");
-        } else {
-            System.out.println("--- Borrowing History (LIFO) ---");
-            for (int i = stack.size() - 1; i >= 0; i--) {
-                Book b = stack.get(i);
-                System.out.println("[ISBN: " + b.getIsbn() + "] " + b.getTitle());
-            }
-        }
-    }
-}
+    // methods must have : push(), pop(), show(), peek()
+} borrow stak.java
