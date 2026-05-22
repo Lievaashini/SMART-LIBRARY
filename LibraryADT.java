25bc43f1c646947ea193fb5e689e76e33d4fcd89
Tigitra Jay <22113137@siswa.um.edu.my>
2026-05-22 22:17:33 +0800

    Added searchBookByTitle to LibraryADT

--- LibraryADT.java
+++ LibraryADT.java
@@ -1,6 +1,7 @@
 public interface LibraryADT {
     void addBook(int i, String t, String a);
     void searchBook(int i);
+    void searchBookByTitle(String title);
     void borrowBook(int i);
     void returnBook(int i, String t, String a);
     void viewLatestHistory();
